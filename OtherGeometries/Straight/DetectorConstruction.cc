a3ef40d98987c136e570809e5fd49275e6e5b765
Edward Brash <brash@jlab.org>
2024-06-21 13:35:13 -0400

    Updates

--- OtherGeometries/Straight/DetectorConstruction.cc
+++ OtherGeometries/Straight/DetectorConstruction.cc
@@ -89,8 +89,12 @@ DetectorConstruction::DetectorConstruction()
   G4double rindex3[NUM] = {1.52, 1.52, 1.52, 1.52, 1.52, 1.52};
   G4double reflectivity[NUM] = {0.3, 0.3, 0.3, 0.3, 0.3, 0.3};
 
-  G4double reflectivity2[NUM] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+  G4double reflectivity2[NUM] = {1., 1., 1., 1., 1., 1.};
+  //G4double reflectivity2[NUM] = {0.9999999, 0.9999999, 0.9999999, 0.9999999, 0.9999999, 0.9999999};
+  //G4double reflectivity2[NUM] = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
   G4double tran2[NUM] = {0., 0., 0., 0., 0., 0.};
+  //G4double tran2[NUM] = {0.0000001, 0.0000001, 0.0000001, 0.0000001, 0.0000001, 0.0000001};
+  //G4double tran2[NUM] = {0.9, 0.9, 0.9, 0.9, 0.9, 0.9};
 
   G4double tran[NUM] = {0.7, 0.7, 0.7, 0.7, 0.7, 0.7};
   G4double absorption[NUM] = {3.448*m, 4.082 * m,  6.329 * m,  9.174 * m,  12.346 * m, 13.889 * m};
@@ -101,6 +105,8 @@ DetectorConstruction::DetectorConstruction()
 
   fSurfaceMPT2->AddProperty("REFLECTIVITY", pp, reflectivity2, NUM);
   fSurfaceMPT2->AddProperty("TRANSMITTANCE", pp, tran2, NUM);
+  fSurfaceMPT->AddProperty("REFLECTIVITY", pp, reflectivity2, NUM);
+  fSurfaceMPT->AddProperty("TRANSMITTANCE", pp, tran2, NUM);
 
   fWorldMPT->AddProperty("RINDEX", pp, rindex2, NUM);
   fScintMPT->AddProperty("RINDEX", pp, rindex3, NUM);
