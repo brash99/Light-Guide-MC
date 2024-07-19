fc52584a231dd33bfd239ba3e38683fd6590b690
Edward Brash <brash@jlab.org>
2024-07-19 06:52:51 -0400

    Updates

--- CMakeFiles/OpNovice2.dir/DependInfo.cmake
+++ CMakeFiles/OpNovice2.dir/DependInfo.cmake
@@ -8,27 +8,23 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/brash/Light-Guide-MC/OpNovice2.cc" "CMakeFiles/OpNovice2.dir/OpNovice2.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/OpNovice2.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/ActionInitialization.cc" "CMakeFiles/OpNovice2.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/ActionInitialization.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/DetectorConstruction.cc" "CMakeFiles/OpNovice2.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/DetectorConstruction.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/DetectorMessenger.cc" "CMakeFiles/OpNovice2.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/DetectorMessenger.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/EventAction.cc" "CMakeFiles/OpNovice2.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/EventAction.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/HistoManager.cc" "CMakeFiles/OpNovice2.dir/src/HistoManager.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/HistoManager.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/PrimaryGeneratorAction.cc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorAction.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/PrimaryGeneratorMessenger.cc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorMessenger.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorMessenger.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/Run.cc" "CMakeFiles/OpNovice2.dir/src/Run.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/Run.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/RunAction.cc" "CMakeFiles/OpNovice2.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/RunAction.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/SteppingAction.cc" "CMakeFiles/OpNovice2.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/SteppingAction.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/TrackInformation.cc" "CMakeFiles/OpNovice2.dir/src/TrackInformation.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/TrackInformation.cc.o.d"
-  "/Users/brash/Light-Guide-MC/src/TrackingAction.cc" "CMakeFiles/OpNovice2.dir/src/TrackingAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/TrackingAction.cc.o.d"
+  "/home/brash/Light-Guide-MC/OpNovice2.cc" "CMakeFiles/OpNovice2.dir/OpNovice2.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/OpNovice2.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/ActionInitialization.cc" "CMakeFiles/OpNovice2.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/ActionInitialization.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/DetectorConstruction.cc" "CMakeFiles/OpNovice2.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/DetectorConstruction.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/DetectorMessenger.cc" "CMakeFiles/OpNovice2.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/DetectorMessenger.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/EventAction.cc" "CMakeFiles/OpNovice2.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/EventAction.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/HistoManager.cc" "CMakeFiles/OpNovice2.dir/src/HistoManager.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/HistoManager.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/PrimaryGeneratorAction.cc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/PrimaryGeneratorMessenger.cc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorMessenger.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/PrimaryGeneratorMessenger.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/Run.cc" "CMakeFiles/OpNovice2.dir/src/Run.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/Run.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/RunAction.cc" "CMakeFiles/OpNovice2.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/RunAction.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/SteppingAction.cc" "CMakeFiles/OpNovice2.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/SteppingAction.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/TrackInformation.cc" "CMakeFiles/OpNovice2.dir/src/TrackInformation.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/TrackInformation.cc.o.d"
+  "/home/brash/Light-Guide-MC/src/TrackingAction.cc" "CMakeFiles/OpNovice2.dir/src/TrackingAction.cc.o" "gcc" "CMakeFiles/OpNovice2.dir/src/TrackingAction.cc.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/brash/Light-Guide-MC
+# Install script for directory: /home/brash/Light-Guide-MC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "/home/brash/geant4_workdir")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,48 +27,48 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set path to fallback-tool for dependency-resolution.
+# Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/brash/Light-Guide-MC/OpNovice2")
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2" AND
      NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2")
-    execute_process(COMMAND /Users/brash/anaconda3/bin/install_name_tool
-      -delete_rpath "/Users/brash/Geant4/geant4_install_11.2.1/lib"
-      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/brash/Light-Guide-MC/OpNovice2")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2"
+         OLD_RPATH "/home/brash/geant4-11.2.1-install/lib:"
+         NEW_RPATH "")
     if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" -u -r "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2")
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/OpNovice2")
     endif()
   endif()
 endif()
 
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  include("/Users/brash/Light-Guide-MC/CMakeFiles/OpNovice2.dir/install-cxx-module-bmi-noconfig.cmake" OPTIONAL)
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
-  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-  else()
-    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
-    unset(CMAKE_INST_COMP_HASH)
-  endif()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/brash/Light-Guide-MC/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/brash/Light-Guide-MC/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
